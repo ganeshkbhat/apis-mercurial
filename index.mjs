1c7244140e655d7c548048009c13f8a3cc716c45
Ganesh K Bhat <ganeshsurfs@gmail.com>
2022-11-01 16:51:01 +0530

    [mercurial-apis] [improvement] [bug] [docs] [feature] Adding minor changes to require files. Adding minor changes to mercurial-apis docs

---
+++ index.mjs
@@ -0,0 +1,16 @@
+/**
+ * 
+ * Package: mercurial-apis
+ * Author: Ganesh B
+ * Description: 
+ * Install: npm i mercurial-apis --save
+ * Github: https://github.com/ganeshkbhat/apis-mercurial
+ * npmjs Link: https://www.npmjs.com/package/mercurial-apis
+ * File: index.mjs
+ * File Description: 
+ * 
+*/
+
+/* eslint no-console: 0 */
+
+'use strict';
